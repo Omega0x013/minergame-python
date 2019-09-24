1eb20cb8486f145f948e9406f0b0d68c3cdb8c54
Omega0x013 <xanderbielby@gmail.com>
2019-09-24 18:02:13 +0100

    add lib

---
+++ lib/spawn_probabilities.py
@@ -0,0 +1,157 @@
+class BlockType:
+    def __init__(self, top_color, side_color, health, value):
+        self.top_color = top_color
+        self.side_color = side_color
+        self.health = health / 10
+        self.value = value
+class GroundType:
+    def __init__(self, color):
+        self.color = color
+stone_block = BlockType((0x80, 0x80, 0x80), (0x40, 0x40, 0x40), health=30, value=0)
+emerald_block = BlockType((54, 206, 68), (54 / 2, 206 / 2, 68 / 2), health=90, value=50)
+sapphire_block = BlockType((0, 0, 200), (0, 0, 100), health=60, value=30)
+ruby_block = BlockType((200, 0, 0), (100, 0, 0), health=50, value=20)
+coal_block = BlockType((40, 40, 40), (20, 20, 20), health=40, value=10)
+air_block = BlockType((0, 0, 0, 0), (0, 0, 0, 0), health=0, value=0)
+shopkeeper_block = BlockType((101, 28, 130), (53, 14, 68), health=0, value=0)
+block_types = [stone_block, air_block, emerald_block, sapphire_block, ruby_block, coal_block, shopkeeper_block]
+dirt_ground = GroundType((0x95, 0x4B, 0x33))
+stone_ground = GroundType((77, 77, 77))
+spawn_ground = GroundType((128, 204, 255))
+ground_types = [dirt_ground, stone_ground, spawn_ground]
+rings = [
+    [
+        (0.2, air_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.15, air_block),
+        (0.01, ruby_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.10, air_block),
+        (0.02, ruby_block),
+        (0.01, sapphire_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.05, air_block),
+        (0.03, ruby_block),
+        (0.02, sapphire_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.02, ruby_block),
+        (0.03, sapphire_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.01, ruby_block),
+        (0.03, coal_block),
+        (0.05, sapphire_block),
+        (0.005, emerald_block)
+    ],
+    [
+        (0.02, ruby_block),
+        (0.03, sapphire_block),
+        (0.01, emerald_block)
+    ],
+    [
+        (0.01, ruby_block),
+        (0.02, sapphire_block),
+        (0.02, emerald_block)
+    ],
+    [
+        (0.01, sapphire_block),
+        (0.03, emerald_block)
+    ],
+    [
+        (0.005, sapphire_block),
+        (0.04, emerald_block),
+    ],
+    [
+        (0.05, emerald_block)
+    ],
+    [
+        (0.06, emerald_block)
+    ],
+    [
+        (0.07, emerald_block)
+    ],
+    [
+        (0.08, emerald_block)
+    ],
+    [
+        (0.1, emerald_block),
+        (0.05, sapphire_block),
+        (0.03, ruby_block),
+        (0.01, coal_block)
+    ],
+    [
+        (0.125, emerald_block),
+        (0.075, sapphire_block),
+        (0.05, ruby_block),
+        (0.025, coal_block)
+    ],
+    [
+        (0.15, emerald_block),
+        (0.1, sapphire_block),
+        (0.075, ruby_block),
+        (0.05, coal_block)
+    ],
+    [
+        (0.175, emerald_block),
+        (0.125, sapphire_block),
+        (0.1, ruby_block),
+        (0.075, coal_block)
+    ],
+    [
+        (0.2, emerald_block),
+        (0.15, sapphire_block),
+        (0.125, ruby_block),
+        (0.1, coal_block)
+    ],
+    [
+        (0.2, emerald_block),
+        (0.175, sapphire_block),
+        (0.15, ruby_block),
+        (0.125, coal_block)
+    ],
+    [
+        (0.2, emerald_block),
+        (0.2, sapphire_block),
+        (0.175, ruby_block),
+        (0.15, coal_block)
+    ],
+    [
+        (0.2, emerald_block),
+        (0.2, sapphire_block),
+        (0.2, ruby_block),
+        (0.175, coal_block)
+    ],
+    [
+        (0.2, emerald_block),
+        (0.2, sapphire_block),
+        (0.2, ruby_block),
+        (0.2, coal_block)
+    ],
+    [
+        (0.25, emerald_block),
+        (0.25, sapphire_block),
+        (0.25, ruby_block),
+        (0.25, coal_block)
+    ],
+    [
+        (0.33, emerald_block),
+        (0.33, sapphire_block),
+        (0.33, ruby_block)
+    ],
+    [
+        (0.5, emerald_block),
+        (0.5, sapphire_block)
+    ],
+    [
+        (1, emerald_block)
+    ]
+]
--- play.py
+++ play.py
@@ -2,6 +2,7 @@
 import random
 import math
 import collections
+from lib.spawn_probabilities import *
 
 pygame.font.init()
 
@@ -13,7 +14,7 @@ zone_size_in_blocks = 9
 zone_size = block_size * zone_size_in_blocks
 move_distance = 3
 diagonal_move_distance = move_distance * math.cos(math.radians(45))
-fnt = pygame.font.Font('/home/xander/Documents/openevening/minergame/font.otf', 20)
+fnt = pygame.font.Font('./font.otf', 20)
 screen_size = block_size * 14
 
 zones_with_modifications = {}
@@ -32,170 +33,6 @@ damage_levels = [get_image('/home/xander/Documents/openevening/minergame/graphic
 
 
 # player_movement = [get_image()]
-
-
-class BlockType:
-    def __init__(self, top_color, side_color, health, value):
-        self.top_color = top_color
-        self.side_color = side_color
-        self.health = health / 10
-        self.value = value
-
-
-class GroundType:
-    def __init__(self, color):
-        self.color = color
-
-
-stone_block = BlockType((0x80, 0x80, 0x80), (0x40, 0x40, 0x40), health=30, value=0)
-emerald_block = BlockType((54, 206, 68), (54 / 2, 206 / 2, 68 / 2), health=90, value=50)
-sapphire_block = BlockType((0, 0, 200), (0, 0, 100), health=60, value=30)
-ruby_block = BlockType((200, 0, 0), (100, 0, 0), health=50, value=20)
-coal_block = BlockType((40, 40, 40), (20, 20, 20), health=40, value=10)
-air_block = BlockType((0, 0, 0, 0), (0, 0, 0, 0), health=0, value=0)
-shopkeeper_block = BlockType((101, 28, 130), (53, 14, 68), health=0, value=0)
-block_types = [stone_block, air_block, emerald_block, sapphire_block, ruby_block, coal_block, shopkeeper_block]
-dirt_ground = GroundType((0x95, 0x4B, 0x33))
-stone_ground = GroundType((77, 77, 77))
-spawn_ground = GroundType((128, 204, 255))
-ground_types = [dirt_ground, stone_ground, spawn_ground]
-
-rings = [
-    [
-        (0.2, air_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.15, air_block),
-        (0.01, ruby_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.10, air_block),
-        (0.02, ruby_block),
-        (0.01, sapphire_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.05, air_block),
-        (0.03, ruby_block),
-        (0.02, sapphire_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.02, ruby_block),
-        (0.03, sapphire_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.01, ruby_block),
-        (0.03, coal_block),
-        (0.05, sapphire_block),
-        (0.005, emerald_block)
-    ],
-    [
-        (0.02, ruby_block),
-        (0.03, sapphire_block),
-        (0.01, emerald_block)
-    ],
-    [
-        (0.01, ruby_block),
-        (0.02, sapphire_block),
-        (0.02, emerald_block)
-    ],
-    [
-        (0.01, sapphire_block),
-        (0.03, emerald_block)
-    ],
-    [
-        (0.005, sapphire_block),
-        (0.04, emerald_block),
-    ],
-    [
-        (0.05, emerald_block)
-    ],
-    [
-        (0.06, emerald_block)
-    ],
-    [
-        (0.07, emerald_block)
-    ],
-    [
-        (0.08, emerald_block)
-    ],
-    [
-        (0.1, emerald_block),
-        (0.05, sapphire_block),
-        (0.03, ruby_block),
-        (0.01, coal_block)
-    ],
-    [
-        (0.125, emerald_block),
-        (0.075, sapphire_block),
-        (0.05, ruby_block),
-        (0.025, coal_block)
-    ],
-    [
-        (0.15, emerald_block),
-        (0.1, sapphire_block),
-        (0.075, ruby_block),
-        (0.05, coal_block)
-    ],
-    [
-        (0.175, emerald_block),
-        (0.125, sapphire_block),
-        (0.1, ruby_block),
-        (0.075, coal_block)
-    ],
-    [
-        (0.2, emerald_block),
-        (0.15, sapphire_block),
-        (0.125, ruby_block),
-        (0.1, coal_block)
-    ],
-    [
-        (0.2, emerald_block),
-        (0.175, sapphire_block),
-        (0.15, ruby_block),
-        (0.125, coal_block)
-    ],
-    [
-        (0.2, emerald_block),
-        (0.2, sapphire_block),
-        (0.175, ruby_block),
-        (0.15, coal_block)
-    ],
-    [
-        (0.2, emerald_block),
-        (0.2, sapphire_block),
-        (0.2, ruby_block),
-        (0.175, coal_block)
-    ],
-    [
-        (0.2, emerald_block),
-        (0.2, sapphire_block),
-        (0.2, ruby_block),
-        (0.2, coal_block)
-    ],
-    [
-        (0.25, emerald_block),
-        (0.25, sapphire_block),
-        (0.25, ruby_block),
-        (0.25, coal_block)
-    ],
-    [
-        (0.33, emerald_block),
-        (0.33, sapphire_block),
-        (0.33, ruby_block)
-    ],
-    [
-        (0.5, emerald_block),
-        (0.5, sapphire_block)
-    ],
-    [
-        (1, emerald_block)
-    ]
-]
 
 up = 0
 left = 1
@@ -514,7 +351,7 @@ def mainloop():
         # Draw UI
         # screen.blit(fnt.render(str(int(clock.get_fps())), False, (255, 255, 255)), (0, 0))
         pygame.draw.rect(screen, (0, 0, 0), (0, 0, 150, 150))
-        screen.blit(fnt.render('$' + str(gold), True, (255, 255, 0)), (0, 0))
+        screen.blit(fnt.render("u " + str(gold), True, (255, 255, 0)), (0, 0))
         ui_y = 30
         for block_type in block_types:
             if block_type.value > 0:
@@ -531,5 +368,5 @@ pygame.init()
 pygame.font.init()
 screen = pygame.display.set_mode((screen_size, screen_size))
 pygame.display.set_caption('Mining Game')
-pygame.display.set_icon(pygame.image.load('/home/xander/Documents/openevening/minergame/icon.png'))
+pygame.display.set_icon(pygame.image.load('./icon.png'))
 mainloop()
